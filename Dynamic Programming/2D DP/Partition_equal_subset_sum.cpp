f4f37f8820080843a02afcb9099ece1949760388
rishabh27goel <rishabhgoel2705@gmail.com>
2022-10-23 21:34:23 +0530

    23-oct-update

---
+++ Dynamic Programming/2D DP/Partition_equal_subset_sum.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    cout << "Enter size : ";
+    int size;
+    cin >> size;
+
+    cout << "Enter array : ";
+    vector <int> nums(size);
+
+    
+
+
+    cout << endl;
+    return 0;
+}
