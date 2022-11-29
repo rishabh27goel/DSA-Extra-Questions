cf8bb022780c758a0c4d8d6a29495721b42677db
Rishabh Goel <rishabhgoel2705@gmail.com>
2022-11-29 22:08:49 +0530

    29-nov-update

--- String/Knuth_Morris_Pratt.cpp
+++ String/Knuth_Morris_Pratt.cpp
@@ -1,27 +1,110 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 // Method 1 : Brute Force
 // Time Complexity : O(n * p)  Space Complexity : O(1)
+// bool searchPattern(string &input, string &pattern){
+
+//     int n = input.size();
+//     int p = pattern.size();
+
+//     for(int i=0; i<n; i++){
+
+//         int j = 0;
+
+//         while(j < p){
+
+//             if(input[i+j] != pattern[j])
+//                 break;
+
+//             j++;
+//         }
+
+//         if(j == p)
+//             return true;
+//     }
+
+//     return false;
+// }
+
+// Method 2 : KMP Algorithm
+// Time Complexity : O(n * p)  Space Complexity : O(1)
+vector<int> computeLPS(string &pattern){
+
+    int m = pattern.size();
+
+    // Initialize LPS
+    vector<int> lps(m, 0);
+
+    int len = 0;
+    int i = 1;
+
+    while(i < m){
+
+        if(pattern[len] == pattern[i]){
+
+            len++;
+            lps[i] = len;
+            
+            i++;
+        }
+        else{
+
+            // If length is not zero then check previous
+            if(len != 0){
+
+                len = lps[len-1];
+            }
+            else{
+
+                lps[i] = 0;
+                i++;
+            }
+        }
+    }
+
+    return lps;
+}
+
 bool searchPattern(string &input, string &pattern){
 
     int n = input.size();
-    int p = pattern.size();
+    int m = pattern.size();
 
-    for(int i=0; i<n; i++){
+    // Create LPS Array [ Longest Prefix Suffix ]
+    vector<int> lps = computeLPS(pattern);
 
-        int j = 0;
 
-        while(j < p){
+    // Search pattern in input string
+    int i = 0;
+    int j = 0;
 
-            if(input[i+j] != pattern[j])
-                break;
+    while(i < n - m + 1){
 
+        // If match is found
+        if(input[i] == pattern[j]){
+
+            i++;
             j++;
         }
+        else{
 
-        if(j == p)
-            return true;
+            if(j != 0){
+
+                j = lps[j-1];
+            }
+            else{
+
+                i++;
+            }
+        }
+        
+        if(j == m){
+
+            cout << "Match Found at index " << i - j << endl;
+            j = lps[j-1];
+        }
     }
 
     return false;
