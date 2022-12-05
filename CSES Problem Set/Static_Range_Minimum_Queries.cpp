7e2795ead44db503535f79d37fbcdd7e66f7505c
Rishabh Goel <rishabhgoel2705@gmail.com>
2022-12-05 22:16:16 +0530

    5-dec-update

--- CSES Problem Set/Static_Range_Minimum_Queries.cpp
+++ CSES Problem Set/Static_Range_Minimum_Queries.cpp
@@ -1,51 +1,11 @@
-// #include <iostream>
-// #include <vector>
-// using namespace std;
+#include <iostream>
+#include <vector>
+#include <climits>
+using namespace std;
 
 #define ll long long int
 
-// int main()
-// {
-//     ll n, q;
-//     cin >> n >> q;
-
-//     vector<ll> arr(n);
-
-//     vector<ll> prefix(n);
-//     long sum = 0;
-
-//     for(ll i=0; i<n; i++){
-
-//         cin >> arr[i];
-    
-//         sum += arr[i];
-//         prefix[i] = sum;
-//     }
-
-//     for(ll j=0; j<q; j++){
-
-//         ll a, b;
-//         cin >> a >> b;
-
-//         ll result;
-
-//         if(a - 2 >= 0)
-//             result = prefix[b-1] - prefix[a - 2];
-//         else    
-//             result = prefix[b-1];
-
-//         cout << result << endl;
-//     }
-
-//     cout << endl;
-//     return 0;
-// }
-
-#include <iostream>
-#include <vector>
-using namespace std;
-
-void createSegmentTree(ll node, vector<ll> &arr, int start, int end, vector<ll> &tree){
+void createSegmentTree(ll node, vector<ll> &arr, ll start, ll end, vector<ll> &tree){
 
     if(start == end){
 
@@ -62,7 +22,7 @@ void createSegmentTree(ll node, vector<ll> &arr, int start, int end, vector<ll> 
         createSegmentTree(2 * node + 1, arr, mid+1, end, tree);
 
         // Store values of left and right tree
-        tree[node] = tree[2 * node] + tree[2 * node + 1];
+        tree[node] = min(tree[2 * node], tree[2 * node + 1]);
     }
 }
 
@@ -70,7 +30,7 @@ ll query(ll node, vector<ll> &tree, ll start, ll end, ll l, ll r){
 
     // Base Case
     if(end < l || start > r)
-        return 0;
+        return INT_MAX;
 
     // If node is in range
     if(l <= start && end <= r)
@@ -83,7 +43,7 @@ ll query(ll node, vector<ll> &tree, ll start, ll end, ll l, ll r){
     ll part2 = query(2 * node + 1, tree, mid+1, end, l, r);
     
 
-    return part1 + part2;
+    return min(part1, part2);
 }
 
 int main()
@@ -93,19 +53,19 @@ int main()
 
     vector<ll> arr(n);
 
-    for(int i=0; i<n; i++){
+    for(ll i=0; i<n; i++){
 
         cin >> arr[i];
     }
 
     // Create Segment Tree
-    vector<ll> tree(n * 2, 0);
+    vector<ll> tree(n * 2, INT_MAX);
 
     createSegmentTree(1, arr, 0, n-1, tree);
 
 
     // Queries
-    for(int i=0; i<q; i++){
+    for(ll i=0; i<q; i++){
 
         ll l, r;
         cin >> l >> r;
