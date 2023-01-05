bdff87861b129e102bc4dab349ef81183f47cf16
Rishabh Goel <rishabhgoel2705@gmail.com>
2023-01-05 23:22:14 +0530

    5-jan-update

---
+++ Array/Find_the_element_that_appears_once.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    cout << "Enter n : ";
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    
+
+
+    cout << endl;
+    return 0;
+}
