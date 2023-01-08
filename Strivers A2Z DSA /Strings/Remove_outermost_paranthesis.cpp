713888e618c86a40d66ee18648fdee5de84191fe
Rishabh Goel <rishabhgoel2705@gmail.com>
2023-01-08 23:36:41 +0530

    8-jan-update

--- Strivers A2Z DSA /Strings/Remove_outermost_paranthesis.cpp
+++ Strivers A2Z DSA /Strings/Remove_outermost_paranthesis.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
 using namespace std;
 
+// Method 1 : Using Stack
+// Time Complexity : O(n)   Space Complexity : O(n)
+string removeOutermostParenthesis(string &input){
+
+    int n = input.size();
+
+    
+}
+
+
 int main()
 {
-    
+    cout << "Enter input string : ";
+    string input;
+    cin >> input;
 
+    string str = removeOutermostParenthesis(input);
+
+
+    cout << endl;
     return 0;
 }
