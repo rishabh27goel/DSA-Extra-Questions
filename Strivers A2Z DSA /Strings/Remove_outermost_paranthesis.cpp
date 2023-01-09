abbff7b877aff2c100e2da41c8f4a3a9b531a88b
Rishabh Goel <rishabhgoel2705@gmail.com>
2023-01-09 13:58:11 +0530

    9-jan-update

--- Strivers A2Z DSA /Strings/Remove_outermost_paranthesis.cpp
+++ Strivers A2Z DSA /Strings/Remove_outermost_paranthesis.cpp
@@ -1,15 +1,78 @@
 #include <iostream>
+#include <stack>
 using namespace std;
 
 // Method 1 : Using Stack
 // Time Complexity : O(n)   Space Complexity : O(n)
+// string removeOutermostParenthesis(string &input){
+
+//     int n = input.size();
+
+//     string result = "";
+//     stack<char> st;
+
+//     for(int i=0; i<input.size(); i++){
+
+//         if(input[i] == '('){
+
+//             // Atleast 1 '(' in the stack
+//             if(st.size() > 0){
+
+//                 result += '(';
+//             }
+
+//             st.push('(');
+//         }
+//         else{
+
+//             // Atleast 2 '(' in the stack
+//             if(st.size() > 1){
+
+//                 result += ')';
+//             }
+
+//             st.pop();
+//         }
+//     }
+
+//     return result;
+// }
+
+// Method 2 : Without Extra Space
+// Time Complexity : O(n)   Space Complexity : O(1)
 string removeOutermostParenthesis(string &input){
 
     int n = input.size();
 
-    
-}
+    string result = "";
+    int opening = 0;
 
+    for(int i=0; i<input.size(); i++){
+
+        if(input[i] == '('){
+
+            // Atleast 1 '(' in the stack
+            if(opening > 0){
+
+                result += '(';
+            }
+
+            opening++;
+        }
+        else{
+
+            // Atleast 2 '(' in the stack
+            if(opening > 1){
+
+                result += ')';
+            }
+
+            opening--;
+        }
+    }
+
+    return result;
+}
 
 int main()
 {
@@ -18,6 +81,8 @@ int main()
     cin >> input;
 
     string str = removeOutermostParenthesis(input);
+
+    cout << "Optimised String : " << str;
 
 
     cout << endl;
