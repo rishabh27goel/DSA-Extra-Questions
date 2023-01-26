72c23b59dee0ce6f30b048f705c15a156d3dabc2
Rishabh Goel <rishabhgoel2705@gmail.com>
2023-01-26 23:10:07 +0530

    26-jan-update

--- String/Longest_Prefix_Suffix.cpp
+++ String/Longest_Prefix_Suffix.cpp
@@ -1,36 +1,74 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 // Method 1 : Brute Force
 // Time Complexity : O(n ^ 2)  Space Complexity : O(1)
+// string findLongestPrefixSuffix(string &str){
+
+//     int n = str.size();
+
+//     for(int len=n-1; len>=0; len--){
+
+//         int i = 0;
+//         int j = n - len;
+
+//         bool found = true;
+
+//         while(j < n){
+
+//             if(str[i] != str[j]){
+
+//                 found = false;
+//                 break;
+//             }
+
+//             i++;
+//             j++;
+//         }
+
+//         if(found)
+//             return str.substr(0, len);
+//     }
+
+//     return "";
+// }
+
+// Method 2 : Using LPS Array [KMP Algorithm]
+// Time Complexity : O(n)  Space Complexity : O(n)
 string findLongestPrefixSuffix(string &str){
 
     int n = str.size();
 
-    for(int len=n-1; len>=0; len--){
+    vector<int> lps(n, 0);
 
-        int i = 0;
-        int j = n - len;
+    int j = 1;
+    int len = 0;
 
-        bool found = true;
+    while(j < n){
 
-        while(j < n){
+        if(str[len] == str[j]){
 
-            if(str[i] != str[j]){
-
-                found = false;
-                break;
-            }
-
-            i++;
+            len++;
+            lps[j] = len;
+            
             j++;
         }
+        else{
 
-        if(found)
-            return str.substr(0, len);
+            if(len == 0){
+
+                lps[j] = 0;
+                j++;
+            }
+            else{
+
+                len = lps[len-1];
+            }
+        }
     }
 
-    return "";
+    return str.substr(0, lps[n-1]);
 }
 
 int main()
