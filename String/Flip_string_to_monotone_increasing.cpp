edbee18bff6853f890fee1ef2c5e4fb84dce2de5
Rishabh Goel <rishabhgoel2705@gmail.com>
2023-02-11 23:38:15 +0530

    11-feb-update

---
+++ String/Flip_string_to_monotone_increasing.cpp
@@ -0,0 +1,184 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+// Method 1 : Using Dynamic Programming
+// Time Complexity : O(2 ^ n)  Space Complexity : O(n)
+// int findCount(int n, string &str, char prev, int ind){
+
+//     // Base Case
+//     if(ind >= n)
+//         return 0;
+
+
+//     // Previous is '0' [we can flip or no change at all]
+//     int first = INT_MAX;
+//     int second = INT_MAX;
+
+//     if(prev == '0'){
+
+//         // Current is '0'
+//         if(str[ind] == '0'){
+
+//             first = findCount(n, str, '0', ind+1);
+//             second = 1 + findCount(n, str, '1', ind+1);
+//         }
+//         else{
+
+//             first = findCount(n, str, '1', ind+1);
+//             second = 1 + findCount(n, str, '0', ind+1);
+//         }
+//     }
+//     // Current is '1'
+//     else{
+
+//         if(str[ind] == '0'){
+
+//             first = 1 + findCount(n, str, '1', ind+1);
+//         }
+//         else{
+
+//             second = findCount(n, str, '1', ind+1);
+//         }
+//     }
+
+//     return min(first, second);
+// }
+
+// int findFlipsCount(string &str){
+
+//     int n = str.size();
+
+//     return findCount(n, str, '0', 0);
+// }
+
+// Method 2 : Using Dynamic Programming & Memoisation
+// Time Complexity : O(2 * n)  Space Complexity : O(2 * n)
+// int findCount(int n, string &str, char prev, int ind, vector<vector<int>> &dp){
+
+//     // Base Case
+//     if(ind >= n)
+//         return 0;
+
+//     if(dp[prev-'0'][ind] != -1)
+//         return dp[prev-'0'][ind];
+
+    
+//     int first = INT_MAX;
+//     int second = INT_MAX;
+
+//     // Previous is '0' [we can flip or no change at all]
+//     if(prev == '0'){
+
+//         // Current is '0'
+//         if(str[ind] == '0'){
+
+//             first = findCount(n, str, '0', ind+1, dp);
+//             second = 1 + findCount(n, str, '1', ind+1, dp);
+//         }
+//         else{
+
+//             first = findCount(n, str, '1', ind+1, dp);
+//             second = 1 + findCount(n, str, '0', ind+1, dp);
+//         }
+//     }
+//     // Previous is '1' [we can flip or no change at all]
+//     else{
+
+//         if(str[ind] == '0'){
+
+//             first = 1 + findCount(n, str, '1', ind+1, dp);
+//         }
+//         else{
+
+//             second = findCount(n, str, '1', ind+1, dp);
+//         }
+//     }
+
+//     return dp[prev-'0'][ind] = min(first, second);
+// }
+
+// int findFlipsCount(string &str){
+
+//     int n = str.size();
+
+//     vector<vector<int>> dp(2, vector<int> (n, -1));
+
+//     return findCount(n, str, '0', 0, dp);
+// }
+
+// Method 3 : Using Dynamic Programming & Tabulation
+// Time Complexity : O(2 * n)  Space Complexity : O(2 * n)
+int findCount(int n, string &str, char prev, int ind, vector<vector<int>> &dp){
+
+    // Base Case
+    if(ind >= n)
+        return 0;
+
+    if(dp[prev-'0'][ind] != -1)
+        return dp[prev-'0'][ind];
+
+    
+    int first = INT_MAX;
+    int second = INT_MAX;
+
+    // Previous is '0' [we can flip or no change at all]
+    if(prev == '0'){
+
+        // Current is '0'
+        if(str[ind] == '0'){
+
+            first = findCount(n, str, '0', ind+1, dp);
+            second = 1 + findCount(n, str, '1', ind+1, dp);
+        }
+        else{
+
+            first = findCount(n, str, '1', ind+1, dp);
+            second = 1 + findCount(n, str, '0', ind+1, dp);
+        }
+    }
+    // Previous is '1' [we can flip or no change at all]
+    else{
+
+        if(str[ind] == '0'){
+
+            first = 1 + findCount(n, str, '1', ind+1, dp);
+        }
+        else{
+
+            second = findCount(n, str, '1', ind+1, dp);
+        }
+    }
+
+    return dp[prev-'0'][ind] = min(first, second);
+}
+
+int findFlipsCount(string &str){
+
+    int n = str.size();
+
+    vector<vector<int>> dp(2, vector<int> (n+1, 0));
+
+    for(int prev=0; prev<2; prev++){
+
+        for(int ind=)
+    }
+
+    return findCount(n, str, '0', 0, dp);
+}
+
+int main()
+{
+    cout << "Enter Input String : ";
+    string str;
+    cin >> str;
+
+    // Count Flips
+    int count = findFlipsCount(str);
+
+    cout << "Flips Count : " << count;
+
+
+    cout << endl;
+    return 0;
+}
