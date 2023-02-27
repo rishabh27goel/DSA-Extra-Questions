d8b75e39044cdd94e95fb26128687d096c9fc43c
Rishabh Goel <rishabhgoel2705@gmail.com>
2023-02-27 23:51:31 +0530

    27-feb-update

--- Array/Minimize_deviation_in_array.cpp
+++ Array/Minimize_deviation_in_array.cpp
@@ -3,33 +3,85 @@
 #include <queue>
 using namespace std;
 
-// Method : Using Priority Queue
+// Method 1 : Using Priority Queue
+// Time Complexity : O(n * log(n))  Space Complexity : O(n)
+// int findMinimumDeviation(vector<int> &arr){
+
+//     int n = arr.size();
+
+//     priority_queue<int> q;
+//     int minDev = INT_MAX;
+//     int minNum = INT_MAX;
+
+//     for(int i=0; i<n; i++){
+
+//         arr[i] & 1 ? q.push(arr[i] * 2) : q.push(arr[i]);   
+//         minNum = arr[i] & 1 ? min(arr[i] * 2, minNum) : min(arr[i], minNum); 
+//     }
+
+//     while(!(q.top() & 1)){
+
+//         int top = q.top();
+//         q.pop();
+//         q.push(top / 2);
+
+//         minDev = min(top - minNum, minDev);
+//         minNum = min(top / 2, minNum);
+//     }
+
+//     return min(q.top() - minNum, minDev);;
+// }
+
+// Method 2 : Sorting the vector
 // Time Complexity : O(n * log(n))  Space Complexity : O(n)
 int findMinimumDeviation(vector<int> &arr){
 
     int n = arr.size();
 
-    priority_queue<int> q;
-    int minDev = INT_MAX;
-    int minNum = INT_MAX;
+    int maxOdd = INT_MIN;
 
     for(int i=0; i<n; i++){
 
-        arr[i] & 1 ? q.push(arr[i] * 2) : q.push(arr[i]);   
-        minNum = arr[i] & 1 ? min(arr[i] * 2, minNum) : min(arr[i], minNum); 
+        int val = arr[i];
+
+        while(val % 2 == 0)
+            val >>= 1;
+
+        maxOdd = max(val, maxOdd);
     }
 
-    while(!(q.top() & 1)){
+    vector<int> tmp;
 
-        int top = q.top();
-        q.pop();
-        q.push(top / 2);
+    for(int i=0; i<n; i++){
 
-        minDev = min(top - minNum, minDev);
-        minNum = min(top / 2, minNum);
+        if(arr[i] % 2 == 1){
+
+            tmp.push_back(arr[i]);
+        }
+        else{
+
+            int val = arr[i];
+            val >>= 1;
+
+            while(val > maxOdd){
+
+                val >>= 1;
+            }
+
+            tmp.push_back(val);
+        }
     }
 
-    return min(q.top() - minNum, minDev);;
+    sort(tmp.begin(), tmp.end());
+
+    int result = tmp[n-1] - tmp[0];
+
+    for(int i=0; i<n-1; i++){
+
+        result = min(result, max(tmp[n-1], tmp[i] * 2) - min(tmp[0] * 2, tmp[i+1]));
+    }
+
+    return result;
 }
 
 int main()
