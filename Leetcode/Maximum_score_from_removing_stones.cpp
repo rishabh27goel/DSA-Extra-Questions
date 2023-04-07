a110ff1fcb4b029338194f1e68073c4d911419d5
Rishabh Goel <rishabhgoel2705@gmail.com>
2023-04-07 22:05:17 +0530

    7-apr-update

--- Leetcode/Maximum_score_from_removing_stones.cpp
+++ Leetcode/Maximum_score_from_removing_stones.cpp
@@ -3,7 +3,26 @@ using namespace std;
 
 int main()
 {
+    int a;
+    cin >> a;
+
+    int b;
+    cin >> b;
+
+    int c;
+    cin >> c;
+
+    // Maximum Score
+    int maxNum = max(a, max(b, c));
+    int minNum = min(a, min(b, c));
+    int midOne = (a + b + c) - maxNum - minNum;
+
+    if(minNum + midOne <= maxNum)
+        cout << "Maximum Score is : " << minNum + midOne;
+    else
+        cout << "Maximum Score is : " << maxNum + ((minNum + midOne - maxNum) / 2);
 
 
+    cout << endl;
     return 0;
 }
