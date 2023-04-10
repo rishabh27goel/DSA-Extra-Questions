d7269eda676b822bd9f83acef4642b8678d9a602
Rishabh Goel <rishabhgoel2705@gmail.com>
2023-04-10 15:31:57 +0530

    10-apr-update

---
+++ Leetcode/Hand_of_straights.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+// Method : Using sorting and hashmap
+// Time Complexity : O(n)  Space Complexity : O(n)
+bool handOfStraights(vector<int> &arr, int groupSize){
+
+    int n = arr.size();
+
+    unordered_map<int, int> mp;
+
+    for(int i=0; i<n; i++){
+
+        mp[arr[i]]++;
+    }
+
+
+    int i = 0;
+
+    while(i < n){
+
+        if(mp[arr[i]] == 0){
+
+            i++;
+            continue;
+        }
+
+        int val = arr[i];
+        int size = groupSize;
+
+        while(size){
+
+            mp[val]--;
+
+            // Not valid
+            if(mp[val] < 0)
+                return false;
+
+            val++;
+            size--;
+        }
+
+        i++;
+    }
+
+    return true;
+}
+
+int main()
+{
+    cout << "Enter size : ";
+    int size;
+    cin >> size;
+
+    vector<int> arr(size);
+
+    for(int i=0; i<size; i++){
+
+        cin >> arr[i];
+    }
+
+    cout << "Enter group size : ";
+    int groupSize;
+    cin >> groupSize;
+
+
+    // Hand of straights
+    bool valid = handOfStraights(arr, groupSize);
+
+    if(valid){
+
+        cout << "Straights Hands !!";
+    }
+    else{
+
+        cout << "Not Straights Hands !!";    
+    }
+
+
+
+    cout << endl;
+    return 0;
+}
