ca6b6b8f65f50c01d843d4d1720b79871bfd3cc7
Rishabh Goel <rishabhgoel2705@gmail.com>
2023-04-14 22:53:17 +0530

    14-apr-update

--- Leetcode/Simplify_Path.cpp
+++ Leetcode/Simplify_Path.cpp
@@ -1,11 +1,54 @@
 #include <iostream>
-#include <stack>
+#include <vector>
+#include <sstream>
+#include <string>
 using namespace std;
+
+// Method : Converting the string into words by a split
+// Time Complexity : O(n)  Space Complexity : O(n)
+string simplifyPath(string &str){
+
+    vector<string> words;
+
+    stringstream ss(str);
+    string word;
+
+    while(getline(ss, word, '/')){
+
+        if(!words.empty() && word == ".."){
+
+            words.pop_back();
+        }
+        else if(word != "" && word != "." && word != ".."){
+
+            words.push_back(word);
+        }
+    }
+
+    if(words.empty())
+        return "/";
+
+    string result = "";
+
+    for(int i=0; i<words.size(); i++){
+
+        if(words[i].size())
+            result += ('/' + words[i]);
+    }
+
+    return result;
+}
 
 int main()
 {
     cout << "Enter Input : ";
+    string str;
+    cin >> str;
 
+    // Simplify Path
+    string path = simplifyPath(str);
+
+    cout << "Path is : " << path;
 
 
     cout << endl;
