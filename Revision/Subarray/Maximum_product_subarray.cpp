4ebe233310c2ea2c29759293b79152199948af33
Rishabh Goel <rishabhgoel2705@gmail.com>
2023-05-30 22:19:43 +0530

    30-may-update

--- Revision/Subarray/Maximum_product_subarray.cpp
+++ Revision/Subarray/Maximum_product_subarray.cpp
@@ -1,10 +1,75 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+// Method 1 : Brute Force
+// Time : O(n^2)  Space : O(1)
+// int findMaxProduct(vector<int> &arr){
+
+//     int n = arr.size();
+
+//     int maxProduct = INT_MIN;
+
+//     for(int i=0; i<n; i++){
+
+//         int product = 1;
+
+//         for(int j=i; j<n; j++){
+
+//             product *= arr[j];
+        
+//             maxProduct = max(product, maxProduct);
+//         }
+//     }
+
+//     return maxProduct;
+// }
+
+// Method 2 : Linear Method
+// Time : O(n)  Space : O(1)
+int findMaxProduct(vector<int> &arr){
+
+    int n = arr.size();
+
+    long maxProduct = INT_MIN;
+
+    long maxNum = INT_MIN;
+    long minNum = INT_MAX;    
+
+    for(int i=0; i<n; i++){
+
+        if(arr[i] < 0)
+            swap(maxNum, minNum);
+    
+
+        maxNum = max((long) arr[i], maxNum * (long) arr[i]);
+        minNum = min((long) arr[i], minNum * (long) arr[i]);
+        
+        maxProduct = max(maxNum, maxProduct);
+    }
+
+    return maxProduct;
+}
 
 int main()
 {
+    cout << "Enter n : ";
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(int i=0; i<n; i++){
+
+        cin >> arr[i];
+    }
 
 
+    int maxProduct = findMaxProduct(arr);
 
+    cout << "Maximum Product Subarray : " << maxProduct;
+
+
+    cout << endl;
     return 0;
 }
