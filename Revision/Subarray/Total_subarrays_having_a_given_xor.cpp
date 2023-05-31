1d29ee152ae18d0d8990e13eba9e684975e2eb0f
Rishabh Goel <rishabhgoel2705@gmail.com>
2023-05-31 22:56:17 +0530

    31-may-update

---
+++ Revision/Subarray/Total_subarrays_having_a_given_xor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    cout << "Enter n : ";
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+
+    for(int )
+
+
+    cout << endl;
+    return 0;
+}
