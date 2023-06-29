2498f46a13b88fcd3deaa9c61648171589ef1039
Rishabh Goel <rishabhgoel2705@gmail.com>
2023-06-29 21:02:24 +0530

    29-jun-update

--- Striver’s SDE Sheet Challenge/Flatten_A_Linked_List.cpp
+++ Striver’s SDE Sheet Challenge/Flatten_A_Linked_List.cpp
@@ -1,9 +1,60 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
-int main()
-{
+class Node {
+    public:
+    	int data;
+ 		Node *next;
+  		Node *child;
+
+ 	Node(int x) : data(x), next(nullptr), child(nullptr) {}
+};
+
+class Compare {
+
+	public:
+		bool operator()(Node* n1, Node* n2){
+
+			return n1->data > n2->data;
+		}
+};
 
 
-    return 0;
+Node* flattenLinkedList(Node *head) {
+
+	Node* newHead = NULL;
+	Node* newTail = NULL;
+
+	priority_queue<Node *, vector<Node *>, Compare> pq;
+	Node *itr = head;
+
+	while (itr != NULL) {
+
+		Node* nextNode = itr->next;
+		itr->next = NULL;
+		pq.push(itr);
+
+		itr = nextNode;
+	}
+
+  	while (!pq.empty()) {
+  
+		Node* top = pq.top();
+		pq.pop();
+
+		if(top->child != NULL){
+
+			pq.push(top->child);
+		}
+
+		if(newHead == NULL)
+			newHead = top;
+		else
+			newTail->child = top;
+
+		newTail = top;
+  	}
+
+	return newHead;
 }
