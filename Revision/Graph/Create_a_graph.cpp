9514402ce7dfc416234b3267224d45a6ff672dc3
Rishabh Goel <rishabhgoel2705@gmail.com>
2023-08-02 22:22:50 +0530

    2-aug-update

--- Revision/Graph/Create_a_graph.cpp
+++ Revision/Graph/Create_a_graph.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <vector>
+#include <queue>
 using namespace std;
 
 class Graph {
@@ -10,7 +12,7 @@ class Graph {
     Graph(int n){
 
         this->n = n;
-        this->adjList = vector<vector<int>> (n);
+        this->adjList = vector< vector<int> > (n);
     }
 
     void addEdge(int u, int v){
@@ -20,6 +22,76 @@ class Graph {
 
         // Undirected
         adjList[v].push_back(u);
+    }
+
+    void dfsHelper(vector<bool> &visited, int start, vector<int> &dfsArr){
+
+        dfsArr.push_back(start);
+        visited[start] = true;
+
+        for(int i=0; i<adjList[start].size(); i++){
+
+            if(visited[adjList[start][i]] == false){
+
+                dfsHelper(visited, adjList[start][i], dfsArr);
+            }
+        }
+    }
+
+    void dfs(){
+
+        vector<bool> visited(n, false);
+        vector<int> dfsArr;
+
+        dfsHelper(visited, 0, dfsArr);
+
+        cout << "\nDFS : ";
+
+        for(int i=0; i<dfsArr.size(); i++){
+
+            cout << dfsArr[i] << " ";
+        }
+    }
+
+    void bfs(){
+
+        vector<bool> visited(n, false);
+        vector<int> bfsArr;
+
+        queue<int> q;
+
+        q.push(0);
+        visited[0] = true;
+
+        while(!q.empty()){
+
+            int size = q.size();
+
+            while(size--){
+
+                int start = q.front();
+                q.pop();
+
+                bfsArr.push_back(start);
+
+                for(int i=0; i<adjList[start].size(); i++){
+
+                    if(visited[adjList[start][i]] == false){
+
+                        q.push(adjList[start][i]);
+                        visited[adjList[start][i]] = true;
+                    }
+                }
+            }
+        }
+
+
+        cout << "\nBFS : ";
+
+        for(int i=0; i<bfsArr.size(); i++){
+
+            cout << bfsArr[i] << " ";
+        }
     }
 };
 
@@ -43,6 +115,9 @@ int main()
 
         g1.addEdge(u, v);
     }
+
+    g1.dfs();
+    g1.bfs();
 
 
     cout << endl;
