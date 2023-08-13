cd85b133f4f180267828bddaddec0a87c20bff7f
Rishabh Goel <rishabhgoel2705@gmail.com>
2023-08-14 00:10:28 +0530

    14-aug-update

--- Revision/Graph/Dijkstra_algorithm.cpp
+++ Revision/Graph/Dijkstra_algorithm.cpp
@@ -1,44 +1,91 @@
 #include <iostream>
 #include <queue>
+#include <set>
 #include <vector>
 using namespace std;
 
-// Using Dijkstra Algorithm
-// Time : O()  Space : O() 
-vector<int>  dijkstraAlgorithm(int n, int m, vector< vector<int> > &edges, int src){
+// // Using Dijkstra Algorithm [Priority Queue]
+// // Time : O(E * log(V))  Space : O(E + V)
+// vector<int> dijkstraAlgorithm(int n, int m, vector< vector<int> > &edges, int src){
+
+//     // Create adjacency list
+//     vector< vector< pair<int, int> > > adjList(n);
+
+//     for(int i=0; i<m; i++){
+
+//         adjList[edges[i][0]].push_back(make_pair(edges[i][1], edges[i][2]));
+//         adjList[edges[i][1]].push_back(make_pair(edges[i][0], edges[i][2]));
+//     }
+
+//     // Use of priority queue
+//     priority_queue< pair<int, int>, vector< pair<int, int> >, greater< pair<int, int> > > pq;
+//     vector<int> shortest(n, INT_MAX);
+    
+//     shortest[src] = 0;
+//     pq.push({ 0, src });
+
+
+//     while(!pq.empty()){
+
+//         pair<int, int> least = pq.top();
+//         pq.pop();
+
+//         int distance = least.first;
+//         int node = least.second;
+
+//         for(int i=0; i<adjList[node].size(); i++){
+
+//             if(shortest[adjList[node][i].first] > distance + adjList[node][i].second){
+
+//                 shortest[adjList[node][i].first] = distance + adjList[node][i].second;
+//                 pq.push({ shortest[adjList[node][i].first],  adjList[node][i].first });
+//             }
+//         }
+//     }
+
+//     return shortest;
+// }
+
+// Using Dijkstra Algorithm [Using Set]
+// Time : O(E * log(V))  Space : O(E + V)
+vector<int> dijkstraAlgorithm(int n, int m, vector<vector<int>> &edges, int src)
+{
 
     // Create adjacency list
-    vector< vector< pair<int, int> > > adjList(n);
+    vector<vector<pair<int, int>>> adjList(n);
 
-    for(int i=0; i<m; i++){
-    
+    for (int i = 0; i < m; i++)
+    {
+
         adjList[edges[i][0]].push_back(make_pair(edges[i][1], edges[i][2]));
         adjList[edges[i][1]].push_back(make_pair(edges[i][0], edges[i][2]));
     }
 
-
-    // Start of priority queue
-    priority_queue< pair<int, int>, vector< pair<int, int> >, greater< pair<int, int> > > pq;
+    // Uisng Set
+    set<pair<int, int>> st;
     vector<int> shortest(n, INT_MAX);
-    
+
     shortest[src] = 0;
-    pq.push({ 0, src });
+    st.insert({0, src});
 
+    while (!st.empty())
+    {
 
-    while(!pq.empty()){
-
-        pair<int, int> least = pq.top();
-        pq.pop();
+        pair<int, int> least = *(st.begin());
 
         int distance = least.first;
         int node = least.second;
 
-        for(int i=0; i<adjList[node].size(); i++){
+        st.erase(least);
 
-            if(shortest[adjList[node][i].second] > distance + adjList[node][i].first){
+        for (int i = 0; i < adjList[node].size(); i++)
+        {
 
-                shortest[adjList[node][i].second] = distance + adjList[node][i].first;
-                pq.push({ shortest[adjList[node][i].second],  adjList[node][i].second });
+            if (shortest[adjList[node][i].first] > distance + adjList[node][i].second)
+            {
+
+                shortest[adjList[node][i].first] = distance + adjList[node][i].second;
+                st.insert({shortest[adjList[node][i].first], adjList[node][i].first});
             }
         }
     }
@@ -56,29 +103,28 @@ int main()
     int m;
     cin >> m;
 
-    vector< vector<int> > edges;
+    vector<vector<int>> edges;
 
-    for(int i=0; i<m; i++){
+    for (int i = 0; i < m; i++)
+    {
 
         int u, v, w;
         cin >> u >> v >> w;
     }
 
-    
     cout << "Enter source node : ";
     int source;
     cin >> source;
-
 
     vector<int> shortest = dijkstraAlgorithm(n, m, edges, source);
 
     cout << "Shortest Distance : ";
 
-    for(int i=0; i<shortest.size(); i++){
+    for (int i = 0; i < shortest.size(); i++)
+    {
 
         cout << shortest[i] << " ";
     }
-
 
     cout << endl;
     return 0;
