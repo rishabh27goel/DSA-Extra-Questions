506a092eab92b06f0b5e7e05aa1365a8e70250ae
Rishabh Goel <rishabhgoel2705@gmail.com>
2024-01-26 20:27:34 +0530

    26-jan-update

---
+++ Leetcode/Out_of_Boundary_Paths.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        
+        return 0;
+    }
+};
