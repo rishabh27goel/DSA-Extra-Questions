f9f73c31f96c776e7360fda87d83059cdf9c5da1
Rishabh Goel <rishabhgoel2705@gmail.com>
2024-01-27 18:15:03 +0530

    27-jan-update

--- Leetcode/Out_of_Boundary_Paths.cpp
+++ Leetcode/Out_of_Boundary_Paths.cpp
@@ -3,8 +3,43 @@ using namespace std;
 
 class Solution {
 public:
-    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+
+    int mod = 1e9 + 7;
+    int dp[51][51][51];
+
+    int findWays(int n, int m, int i, int j, int moves){
+
+        // Base Case
+        if(i < 0 || i >= n || j < 0 || j >= m)
+            return 1;
+
+        if(moves == 0)
+            return 0;
+
+        if(dp[i][j][moves] != -1)
+            return dp[i][j][moves];
+
+
+        int xPos[4] = {-1, 1, 0, 0};
+        int yPos[4] = {0, 0, -1, 1};
+        long long result = 0;        
+
+        for(int k=0; k<4; k++){
+
+            int x = i + xPos[k];
+            int y = j + yPos[k];
+
+            int call = findWays(n, m, x, y, moves-1);
+            result = (result + call) % mod;
+        }
+
+        return dp[i][j][moves] = result % mod;
+    }
+
+    int findPaths(int n, int m, int maxMove, int startRow, int startColumn) {
         
-        return 0;
+        memset(dp, -1, sizeof(dp));
+
+        return findWays(n, m, startRow, startColumn, maxMove);
     }
 };
