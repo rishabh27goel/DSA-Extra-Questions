f13e83cebd64ada079eab435c4f856067d18045b
Rishabh Goel <rishabhgoel2705@gmail.com>
2024-03-06 19:20:19 +0530

    6-mar-update

---
+++ Revision/LinkedList/Linked_List_Cycle.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast != NULL && fast->next != NULL){
+
+            fast = fast->next->next;
+            slow = slow->next;
+
+            if(fast == slow)
+                return true;
+        }
+
+        return false;
+    }
+};
