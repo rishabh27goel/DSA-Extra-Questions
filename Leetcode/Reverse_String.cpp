54fdf912a4079a6c6a146dbfe95dd1f7b0e20b0e
Rishabh Goel <rishabhgoel2705@gmail.com>
2024-06-02 15:15:49 +0530

    2-jun-update

---
+++ Leetcode/Reverse_String.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        
+        int n = s.size();
+
+        int i = 0;
+        int j = n-1;
+
+        while(i < j){
+            swap(s[i], s[j]);
+            i++;
+            j--;
+        }
+    }
+};
