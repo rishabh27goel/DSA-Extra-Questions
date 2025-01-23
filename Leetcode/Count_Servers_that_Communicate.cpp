97cd5d28abc901184898163bebb15b5842a58efd
Rishabh Goel <rishabhgoel2705@gmail.com>
2025-01-23 14:13:46 +0530

    23-jan-update

---
+++ Leetcode/Count_Servers_that_Communicate.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int countServers(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<int> rowCount(n, 0);
+        vector<int> colCount(m, 0);
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j]) {
+                    rowCount[i]++;
+                    colCount[j]++;
+                }
+            }
+        }
+
+        int serverCount = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(grid[i][j]) {
+                    if(rowCount[i] > 1 || colCount[j] > 1) {
+                        serverCount++;
+                    }
+                }
+            }
+        }
+        return serverCount;
+    }
+};
