ff04943e6228280c1e0c9b29876fb24aba66ffc1
Rishabh Goel <rishabhgoel2705@gmail.com>
2025-02-20 17:33:38 +0530

    20-feb-update

--- Leetcode/Find_Unique_Binary_String.cpp
+++ Leetcode/Find_Unique_Binary_String.cpp
@@ -3,89 +3,68 @@
 using namespace std;
 
 class Solution {
-public:
-
-    string findDifferentBinaryString(vector<string>& nums) {
-        
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-
-        int n = nums.size();
-
-        string result = "";
-
-        for(int i=0; i<n; i++){
-
-            result.push_back((nums[i][i] == '0') ? '1' : '0');
-        }
-
-        return result;
-    }
-
-    // int findNumber(string &number){
-
-    //     int result = 0;
-    //     int num = 1;
-
-    //     int j = number.size()-1;
-
-    //     while(j >= 0){
-
-    //         if(number[j] == '1')
-    //             result += num;
-
-    //         num <<= 1;
-    //         j--;
-    //     }
-
-    //     return result;
-    // }
-
-    // string getString(int num, int n){
-
-    //     string result = "";
-    //     int val = 1;
-
-    //     for(int j=0; j<n; j++){
-
-    //         result.push_back((num & val) ? '1' : '0');
-    //         val <<= 1;
-    //     }
-
-    //     reverse(result.begin(), result.end());
-    //     return result;
-    // }
-
-    // string findDifferentBinaryString(vector<string>& nums) {
-        
-    //     ios_base::sync_with_stdio(false);
-    //     cin.tie(NULL);
-    //     cout.tie(NULL);
-
-    //     int n = nums.size();
-
-    //     vector<bool> exists(n + 1, false);
+    public:
+        // int getConvertedNumber(int numSize, string &binaryStr) {
+        //     int currNumber = 0;
+        //     int mask = 1;
     
-    //     for(int i=0; i<n; i++){
-
-    //         int val = findNumber(nums[i]);
-
-    //         if(val < n)
-    //             exists[val] = true;
-    //     }
-
-
-    //     for(int i=0; i<=n; i++){
-
-    //         cout << exists[i] << " ";
-
-    //         if(exists[i] == false){
-
-    //             return getString(i, n);
-    //         }
-    //     }
-
-    //     return "";
-    // }
-};
+        //     for(int idx = numSize - 1; idx >= 0; idx--) {
+        //         if(binaryStr[idx] == '1') {
+        //             currNumber += mask;
+        //         }
+        //         mask <<= 1;
+        //     }
+    
+        //     return currNumber;
+        // }
+    
+        // string findDifferentBinaryString(vector<string>& nums) {
+        //     int numSize = nums.size();
+        //     int size = (1 << numSize);
+    
+        //     vector<bool> visitedNumbers(size, false);
+        //     for(int numIdx = 0; numIdx < numSize; numIdx++) {
+        //         int currNum = getConvertedNumber(numSize, nums[numIdx]);
+        //         visitedNumbers[currNum] = true;
+        //     }
+    
+        //     for(int idx = 0; idx < size; idx++) {
+        //         if(visitedNumbers[idx])  continue;
+    
+        //         string str = "";
+        //         int number = idx;
+    
+        //         for(int bit = numSize - 1; bit >= 0; bit--) {
+        //             str.push_back(((1 << bit) & number) ? '1' : '0');
+        //         }
+    
+        //         return str;
+        //     }
+    
+        //     return "";
+        // }
+    
+        int getConvertedNumber(int numSize, string &binaryStr) {
+            int currNumber = 0;
+            int mask = 1;
+    
+            for(int idx = numSize - 1; idx >= 0; idx--) {
+                if(binaryStr[idx] == '1') {
+                    currNumber += mask;
+                }
+                mask <<= 1;
+            }
+    
+            return currNumber;
+        }
+    
+        string findDifferentBinaryString(vector<string>& nums) {
+            int numSize = nums.size();
+            
+            string binaryStr = "";
+            for(int idx = 0; idx < numSize; idx++) {
+                binaryStr.push_back((nums[idx][idx] == '0') ? '1' : '0');
+            }
+            return binaryStr;
+        }
+    };
