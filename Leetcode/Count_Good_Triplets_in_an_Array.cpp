3f968e79b6d188899e7af5126bd0827cc1a90d70
Rishabh Goel <rishabhgoel2705@gmail.com>
2025-04-15 22:52:07 +0530

    15-apr-update

---
+++ Leetcode/Count_Good_Triplets_in_an_Array.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Solution {
+  public:
+      long long goodTriplets(vector<int>& nums1, vector<int>& nums2) {
+        return 0;
+      }
+  };
