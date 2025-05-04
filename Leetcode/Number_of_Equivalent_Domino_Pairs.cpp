29f5122f2fdf3d809551cc0b9344df64f1075489
Rishabh Goel <rishabhgoel2705@gmail.com>
2025-05-04 23:13:17 +0530

    4-may-update

---
+++ Leetcode/Number_of_Equivalent_Domino_Pairs.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+      int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+        return 0;
+      }
+  };
