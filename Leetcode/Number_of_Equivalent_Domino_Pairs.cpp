411f23f7b618f19f5510bd2af9363cd9c149b06f
Rishabh Goel <rishabhgoel2705@gmail.com>
2025-05-05 14:53:09 +0530

    5-may-update

--- Leetcode/Number_of_Equivalent_Domino_Pairs.cpp
+++ Leetcode/Number_of_Equivalent_Domino_Pairs.cpp
@@ -3,8 +3,21 @@
 using namespace std;
 
 class Solution {
-  public:
-      int numEquivDominoPairs(vector<vector<int>>& dominoes) {
-        return 0;
-      }
-  };
+    public:
+        int numEquivDominoPairs(vector<vector<int>>& dominoes) {
+            int size = dominoes.size();
+    
+            vector<int> count(100, 0);
+            int maxNums = 0;
+    
+            for(int idx = 0; idx < size; idx++) {
+                int one = dominoes[idx][0];
+                int two = dominoes[idx][1];
+    
+                int key = (one < two ? one * 10 + two : two * 10 + one);
+                maxNums += count[key]++;
+            }
+    
+            return maxNums;
+        }
+    };
