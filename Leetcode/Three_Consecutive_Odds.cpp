20f922a9a6415b3975a01aebfaac5a82f3c8ce21
Rishabh Goel <rishabhgoel2705@gmail.com>
2025-05-12 00:09:29 +0530

    12-may-update

--- Leetcode/Three_Consecutive_Odds.cpp
+++ Leetcode/Three_Consecutive_Odds.cpp
@@ -3,19 +3,23 @@
 using namespace std;
 
 class Solution {
-public:
-    bool threeConsecutiveOdds(vector<int>& arr) {
-        
-        int n = arr.size();
-
-        if(n <= 2)
+    public:
+        bool threeConsecutiveOdds(vector<int>& arr) {
+            int size = arr.size();
+    
+            int idx = 0;
+            while(idx < size) {
+                if(arr[idx] % 2 == 0) {
+                    idx++;
+                }
+                else {
+                    int count = 0;
+                    while(idx < size && arr[idx] % 2 == 1) {
+                        count++, idx++;
+                    }
+                    if(count >= 3)  return true;
+                }
+            }
             return false;
-
-        for(int i=0; i<n-2; i++){
-            if((arr[i] & 1) && (arr[i+1] & 1) && (arr[i+2] & 1))
-                return true;
         }
-
-        return false;
-    }
-};
+    };
