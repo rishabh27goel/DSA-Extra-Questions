52ef92e4885123f87cbb666f82e3bd4ea418a36d
Rishabh Goel <rishabhgoel2705@gmail.com>
2025-05-24 20:47:52 +0530

    24-may-update

--- Leetcode/Find_the_Maximum_Sum_of_Node_Values.cpp
+++ Leetcode/Find_the_Maximum_Sum_of_Node_Values.cpp
@@ -4,7 +4,32 @@ using namespace std;
 
 class Solution {
     public:
+        // If number of nodes which can be increased is even - totalSum is fine
+        // Otherwise, check for minimum difference with all possible changes 
+        // (a - b, b - a) find the minimum difference 
         long long maximumValueSum(vector<int>& nums, int k, vector<vector<int>>& edges) {
-            
+            int size = nums.size();
+    
+            long long totalSum = 0;
+            long minNum = INT_MAX;
+            int count = 0;
+    
+            for(int currIdx = 0; currIdx < size; currIdx++) {
+                long currNum = nums[currIdx];
+                if((currNum ^ k) > currNum) {
+                    totalSum += (currNum ^ k);
+                    minNum = min(minNum, (currNum ^ k) - currNum);
+                    count++;
+                }
+                else {
+                    totalSum += currNum;
+                    minNum = min(minNum, currNum - (currNum ^ k));
+                }
+            }
+    
+            if(count % 2 == 0)
+                return totalSum;
+    
+            return totalSum - minNum;
         }
     };
