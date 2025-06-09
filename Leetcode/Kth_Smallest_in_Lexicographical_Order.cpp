699b77bf734c6b09fe1afc9e389bbf270d055ae1
Rishabh Goel <rishabhgoel2705@gmail.com>
2025-06-09 17:40:47 +0530

    9-jun-update

--- Leetcode/Kth_Smallest_in_Lexicographical_Order.cpp
+++ Leetcode/Kth_Smallest_in_Lexicographical_Order.cpp
@@ -3,38 +3,33 @@
 using namespace std;
 
 class Solution {
-public:
-    int subSteps(long n, long start, long end) {
-        int steps = 0;
-        while(start <= n) {
-            steps += (min(n + 1, end) - start);
-
-            start *= 10;
-            end *= 10;
-        }
-        return steps;
-    }
-
-    int findKthNumber(int n, int k) {
-        ios_base::sync_with_stdio(false);
-        // cin.tie(NULL);
-        cout.tie(NULL);
-
-        int counter = 1;
-        k--;
-
-        while(k > 0) {
-            int steps = subSteps(n, counter, counter + 1);
-            if(steps <= k) {
-                counter++;
-                k -= steps;
+    public:
+        int getSteps(long n, long start, long end) {
+            int steps = 0;
+            while(start <= n) {
+                steps += (min(n + 1, end) - start);
+                start *= 10;
+                end *= 10;
             }
-            else {
-                counter *= 10;
-                k--;
-            }
+            return steps;
         }
-
-        return counter;
-    }
-};
+    
+        int findKthNumber(int n, int k) {
+            int counter = 1;
+            k--;
+    
+            while(k > 0) {
+                int steps = getSteps(n, counter, counter + 1);
+                if(steps <= k) {
+                    k -= steps;
+                    counter++;
+                }
+                else {
+                    k--;
+                    counter *= 10;
+                }
+            }
+    
+            return counter;
+        }
+    };
