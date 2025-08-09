7d2c568e7613b9e4a55b0fb63771ce248539d0b1
Rishabh Goel <rishabhgoel2705@gmail.com>
2025-08-09 23:31:22 +0530

    9-aug-update

---
+++ Leetcode/Power_of_Two.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+  public:
+      bool isPowerOfTwo(int n) {
+          if(n <= 0)
+              return false;
+  
+          return (n & (n - 1)) == 0;
+      }
+  };
