dec33d16abd8ca6fc94c1f7382c1d853785d970d
Rishabh Goel <rishabhgoel2705@gmail.com>
2025-09-05 01:43:15 +0530

    5-sep-update

---
+++ Leetcode/Find_Closest_Person.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+  public:
+      int findClosest(int x, int y, int z) {
+          int d1 = abs(x - z);
+          int d2 = abs(y - z);
+          return d1 == d2 ? 0 : (d1 > d2 ? 2 : 1);
+      }
+  };
