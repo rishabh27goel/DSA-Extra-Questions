046167d42cbce90420eb971e9ac1661ac5d66297
Rishabh Goel <rishabhgoel2705@gmail.com>
2025-09-12 22:34:43 +0530

    12-sep-update

--- Leetcode/Sort_Vowels_in_a_String.cpp
+++ Leetcode/Sort_Vowels_in_a_String.cpp
@@ -3,85 +3,40 @@
 using namespace std;
 
 class Solution {
-public:
-
-    bool isVowel(char ch){
-
-        if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
-            return true;
-
-        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
-            return true;
-
-
-        return false;
-    }
-
-    // string sortVowels(string s) {
-        
-    //     int n = s.size();
-
-    //     string vowel = "";
-
-    //     for(int i=0; i<n; i++){
-
-    //         if(isVowel(s[i])){
-
-    //             vowel.push_back(s[i]);
-    //         }
-    //     }
-
-    //     sort(vowel.begin(), vowel.end());
-
-    //     int j = 0;
-
-    //     for(int i=0; i<n; i++){
-
-    //         if(isVowel(s[i])){
-
-    //             s[i] = vowel[j++];
-    //         }
-    //     }
-
-    //     return s;
-    // }
-
-    string sortVowels(string s) {
-        
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-
-        int n = s.size();
-
-        string vowel = "";
-        vector<int> charCount(256, 0);
-
-        for(int i=0; i<n; i++){
-
-            if(isVowel(s[i])) {
-
-                charCount[(int) s[i]]++;
-            }
+    public:
+        bool isVowel(char ch) {
+            if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+                return true;
+    
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                return true;
+    
+            return false;
         }
-
-
-        int j = 0;
-
-        for(int i=0; i<n; i++){
-
-            if(isVowel(s[i])){
-
-                while(j < 256 && charCount[j] == 0){
-
-                    j++;
+        string sortVowels(string str) {
+            ios_base::sync_with_stdio(false);
+            cin.tie(NULL);
+            cout.tie(NULL);
+    
+            int size = str.size();
+    
+            int charCount[128] = {0};
+            for(int idx = 0; idx < size; idx++) {
+                if(isVowel(str[idx])) {
+                    charCount[(int) str[idx]]++;
                 }
-
-                s[i] = (char) j;
-                charCount[j]--;
             }
+    
+            int charIdx = 0;
+            for(int idx = 0; idx < size; idx++) {
+                if(isVowel(str[idx])) {
+                    while(charIdx < 128 && charCount[charIdx] == 0)
+                        charIdx++;
+    
+                    str[idx] = (char) charIdx;
+                    charCount[charIdx]--;
+                }
+            }
+            return str;
         }
-
-        return s;
-    }
-};
+    };
