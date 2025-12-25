a3b34a002c440d20be3e234c633a0e40384fe26c
Rishabh Goel <rishabhgoel2705@gmail.com>
2025-12-26 00:30:28 +0530

    26-dec-update

--- Leetcode/Maximize_Happiness_of_Selected_Children.cpp
+++ Leetcode/Maximize_Happiness_of_Selected_Children.cpp
@@ -3,25 +3,29 @@
 using namespace std;
 
 class Solution {
-public:
-    long long maximumHappinessSum(vector<int>& happiness, int k) {
-        
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
-
-        int n = happiness.size();
-
-        sort(happiness.begin(), happiness.end(), greater<int> ());
-
-        long long result = 0;
-        int c = 0;
-
-        for(int i=0; i<k; i++){
-            result += max(happiness[i] - c, 0);
-            c++;
+    public:
+        long long maximumHappinessSum(vector<int>& happiness, int k) {
+            ios_base::sync_with_stdio(false);
+            cin.tie(NULL);
+            cout.tie(NULL);
+    
+            int size = happiness.size();
+    
+            priority_queue<int> maxHeap;
+            for(int currIdx = 0; currIdx < size; currIdx++)
+                maxHeap.push(happiness[currIdx]);
+    
+            long long totalSum = 0;
+            int numCount = 0;
+    
+            while(k--) {
+                int topValue = maxHeap.top();
+                maxHeap.pop();
+                
+                if(topValue - numCount <= 0)   return totalSum;
+                totalSum += (topValue - numCount);
+                numCount++;
+            }
+            return totalSum;
         }
-
-        return result;
-    }
-};
+    };
